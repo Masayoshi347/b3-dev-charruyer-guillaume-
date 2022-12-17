7425c8c1fa8347e89fb2d948a8f903bd6d8e1536
Masayoshi347 <90193223+Masayoshi347@users.noreply.github.com>
2022-12-17 21:12:08 +0100

    a

--- Main/src/calculatrice.py
+++ Main/src/calculatrice.py
@@ -1,15 +1,30 @@
 # calculatrice.py
 
 def add(a, b):
-    return a + b
+    """Cette fonction retourne la somme de a et b."""
+    result = a + b
+    print(f"{a} + {b} = {result}")
+    return result
 
 def subtract(a, b):
-    return a - b
+    """Cette fonction retourne la différence de a et b (a - b)."""
+    result = a - b
+    print(f"{a} - {b} = {result}")
+    return result
 
 def multiply(a, b):
-    return a * b
+    """Cette fonction retourne le produit de a et b."""
+    result = a * b
+    print(f"{a} * {b} = {result}")
+    return result
 
 def divide(a, b):
+    """Cette fonction retourne le quotient de a et b (a / b).
+    Si b vaut 0, une exception ValueError est levée.
+    """
     if b == 0:
         raise ValueError("Division par zéro")
-    return a / b
+    result = a / b
+    print(f"{a} / {b} = {result}")
+    return result
+
