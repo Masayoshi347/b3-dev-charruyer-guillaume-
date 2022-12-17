6061e1339861fd5086411e7bba24d1d5d9635f0e
Masayoshi347 <90193223+Masayoshi347@users.noreply.github.com>
2022-12-17 20:29:39 +0100

    a

--- Main/test/test_caltulator.py
+++ Main/test/test_caltulator.py
@@ -1,36 +1,26 @@
-import unittest
+# test_calculatrice.py
 
-from calculator import *
+import pytest
+from Main.src.calculatrice import add, subtract, multiply, divide
 
-class TestCalculatrice(unittest.TestCase):
-  def setUp(self):
-    # Code à exécuter avant chaque test
-    pass
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-2, 3) == 1
+    assert add(2, -3) == -1
 
-  def test_addition(self):
-    # Test de l'addition
-    resultat = calculatrice("+", 2, 3)
-    self.assertEqual(resultat, 5)
+def test_subtract():
+    assert subtract(2, 3) == -1
+    assert subtract(-2, 3) == -5
+    assert subtract(2, -3) == 5
 
-  def test_soustraction(self):
-    # Test de la soustraction
-    resultat = calculatrice("-", 2, 3)
-    self.assertEqual(resultat, -1)
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-2, 3) == -6
+    assert multiply(2, -3) == -6
 
-  def test_multiplication(self):
-    # Test de la multiplication
-    resultat = calculatrice("*", 2, 3)
-    self.assertEqual(resultat, 6)
-
-  def test_division(self):
-    # Test de la division
-    resultat = calculatrice("/", 4, 2)
-    self.assertEqual(resultat, 2)
-
-  def test_operation_invalide(self):
-    # Test d'une opération non valide
-    resultat = calculatrice("invalide", 2, 3)
-    self.assertEqual(resultat, "Opération non valide")
-
-# Exécution des tests
-unittest.main()
+def test_divide():
+    assert divide(6, 3) == 2
+    assert divide(-6, 3) == -2
+    assert divide(6, -3) == -2
+    with pytest.raises(ValueError):
+        divide(6, 0)
