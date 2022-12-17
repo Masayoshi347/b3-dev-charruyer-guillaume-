bfb518f6b9d607b85bd8a329a88fd08dcdf8737b
Masayoshi347 <90193223+Masayoshi347@users.noreply.github.com>
2022-12-17 19:14:34 +0100

    Update test_caltulator.py

--- Main/test/test_caltulator.py
+++ Main/test/test_caltulator.py
@@ -1,4 +1,5 @@
 import unittest
+
 from calculator import *
 
 class TestCalculatrice(unittest.TestCase):
@@ -8,27 +9,27 @@ class TestCalculatrice(unittest.TestCase):
 
   def test_addition(self):
     # Test de l'addition
-    resultat = calculator.calculatrice("+", 2, 3)
+    resultat = calculatrice("+", 2, 3)
     self.assertEqual(resultat, 5)
 
   def test_soustraction(self):
     # Test de la soustraction
-    resultat = calculator.calculatrice("-", 2, 3)
+    resultat = calculatrice("-", 2, 3)
     self.assertEqual(resultat, -1)
 
   def test_multiplication(self):
     # Test de la multiplication
-    resultat = calculator.calculatrice("*", 2, 3)
+    resultat = calculatrice("*", 2, 3)
     self.assertEqual(resultat, 6)
 
   def test_division(self):
     # Test de la division
-    resultat = calculator.calculatrice("/", 4, 2)
+    resultat = calculatrice("/", 4, 2)
     self.assertEqual(resultat, 2)
 
   def test_operation_invalide(self):
     # Test d'une opération non valide
-    resultat = calculator.calculatrice("invalide", 2, 3)
+    resultat = calculatrice("invalide", 2, 3)
     self.assertEqual(resultat, "Opération non valide")
 
 # Exécution des tests
